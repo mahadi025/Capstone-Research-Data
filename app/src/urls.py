87daf5dfcf7d477b05981966fe98ba391797cb22
mahadi025 <mahadikarimmunif@gmail.com>
2023-11-01 19:46:32 +0600

    Removed unnecessary imports

--- app/src/urls.py
+++ app/src/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
