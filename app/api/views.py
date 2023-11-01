a81e5a2cfb32e5f0f569fe7b165c534201971839
mahadi025 <mahadikarimmunif@gmail.com>
2023-11-01 16:23:36 +0600

    Fomated some codes

--- app/api/views.py
+++ app/api/views.py
@@ -26,30 +26,3 @@ class GetMethod(viewsets.ModelViewSet):
         "optimizer",
         "dataset",
     ]
-
-    # def get_queryset(self):
-    #     queryset = TransferLearningModel.objects.all()
-    #     model_name = self.request.query_params.get("model_name")
-    #     batch_size = self.request.query_params.get("batch_size")
-    #     learning_rate = self.request.query_params.get("learning_rate")
-    #     epochs = self.request.query_params.get("epochs")
-    #     dropout = self.request.query_params.get("dropout")
-    #     optimizer = self.request.query_params.get("optimizer")
-    #     dataset = self.request.query_params.get("dataset")
-
-    #     if model_name:
-    #         queryset = queryset.filter(model_name=model_name)
-    #     if batch_size:
-    #         queryset = queryset.filter(batch_size=batch_size)
-    #     if learning_rate:
-    #         queryset = queryset.filter(learning_rate=learning_rate)
-    #     if epochs:
-    #         queryset = queryset.filter(epochs=epochs)
-    #     if dropout:
-    #         queryset = queryset.filter(dropout=dropout)
-    #     if dataset:
-    #         queryset = queryset.filter(dataset=dataset)
-    #     if optimizer:
-    #         queryset = queryset.filter(optimizer=optimizer)
-
-    #     return queryset
