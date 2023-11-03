c2eacb1561c87b0b345849fa9dc25df506401fc1
mahadi025 <mahadikarimmunif@gmail.com>
2023-11-03 14:07:54 +0600

    Removed react from django

--- src/settings.py
+++ src/settings.py
@@ -52,7 +52,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, "reactapp/build"),
+            # os.path.join(BASE_DIR, "reactapp/build"),
         ],
         "APP_DIRS": True,
         "OPTIONS": {
@@ -118,9 +118,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "reactapp/build/static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "reactapp/build/static"),
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
