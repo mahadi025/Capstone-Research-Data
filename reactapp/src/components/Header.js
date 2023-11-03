7ec07ac2d54ab51bc4f2b08d049db0dc2535f9a4
mahadi025 <mahadikarimmunif@gmail.com>
2023-11-03 16:35:51 +0600

    Removed reactapp from django

--- reactapp/src/components/Header.js
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-    return (
-        <div className="app-header">
-            <h1>Recorded Data</h1>
-        </div>
-    )
-}
--- src/settings.py
+++ src/settings.py
@@ -51,9 +51,7 @@ ROOT_URLCONF = "src.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [
-            # os.path.join(BASE_DIR, "reactapp/build"),
-        ],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -118,9 +116,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "reactapp/build/static"),
-# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
