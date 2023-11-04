3dcb6d1c480b539527f021e70c904dcbd36bc5b1
mahadi025 <mahadikarimmunif@gmail.com>
2023-11-04 12:13:10 +0600

    Removed unnecessary imports

--- src/urls.py
+++ src/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,7 +9,7 @@ urlpatterns = [
 ]
 
 
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT,
-)
+# urlpatterns += static(
+#     settings.MEDIA_URL,
+#     document_root=settings.MEDIA_ROOT,
+# )
