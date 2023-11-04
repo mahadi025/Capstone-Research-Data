caa837eb29fde39ac236390cb39d5e627ce1ccd5
mahadi025 <mahadikarimmunif@gmail.com>
2023-11-04 12:17:35 +0600

    Added api to the url

--- src/urls.py
+++ src/urls.py
@@ -3,13 +3,15 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("api.urls")),
+    path("api/", include("api.urls")),
 ]
 
 
-# urlpatterns += static(
-#     settings.MEDIA_URL,
-#     document_root=settings.MEDIA_ROOT,
-# )
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
